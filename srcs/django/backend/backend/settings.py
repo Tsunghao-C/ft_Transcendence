c92adf20b177c33ed4fbf433a5e41b584ec713d6
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-11-17 10:25:21 +0100

    disabled cors settings for now

--- srcs/django/backend/backend/settings.py
+++ srcs/django/backend/backend/settings.py
@@ -154,9 +154,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # will need to change this when we get a url
 #CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-CORS_ALLOWED_ORIGINS = [
-	"http://localhost:8000",
-	"http://127.0.0.1:8000",
-	"http://0.0.0.0:8000"
-]
-CORS_ALLOWS_CREDENTIALS = True
+#CORS_ALLOWED_ORIGINS = [
+#	"http://localhost:8000",
+#	"http://127.0.0.1:8000",
+#	"http://0.0.0.0:8000"
+#]
+#CORS_ALLOWS_CREDENTIALS = True
