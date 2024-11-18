1b058ca1ed658d0aaffec91285ad5e27ee98534f
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-11-18 12:52:48 +0100

    gunicorn now serves static files

--- pong-game/game-service/entrypoint.sh
+++ pong-game/game-service/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e  # fail on error
-
 echo "Starting entrypoint script..."
+
+# Create static directory and set permissions
+mkdir -p /app/static && \
+chmod -R 755 /app/static
 
 echo "Waiting for database to be ready..."
 echo "SQL_HOST: ${SQL_HOST}, SQL_PORT: ${SQL_PORT}"
@@ -14,5 +17,11 @@ python manage.py makemigrations || { echo "makemigrations failed"; exit 1; }
 echo "Running migrate..."
 python manage.py migrate || { echo "migrate failed"; exit 1; }
 
+echo "Collecting static files..."
+# Add verbose flag and print directory contents
+python manage.py collectstatic --noinput -v 2 || { echo "static collection failed"; exit 1; }
+echo "Static files directory contents:"
+ls -la /app/static
+
 echo "Starting Gunicorn server..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8004 --log-level debug || { echo "Gunicorn failed"; exit 1; }
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:8004 || { echo "Gunicorn failed"; exit 1; }
