96ed85d88c3f918e3f55a12c9a1234dd64d817a6
Jean Teissier <jteissie@made-f0Br7s5.clusters.42paris.fr>
2024-11-18 14:34:07 +0100

    Updated html page to for basic script
    Add: Skeleton code for game.html page for testing

--- srcs/game/game.js
+++ srcs/game/game.js
@@ -1,6 +1,12 @@
 
 // size by default of the canvas is 300x150, it can be sized arbitrarily by CSS. If it appears distorted, best specify it in the <canvas> attribute and not the CSS
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+if (canvas.getContext)
+{
+	const ctx = canvas.getContext("2d");
+	let ball = {x:canvas.width/2, y:canvas.height/2, color:"black"};
+	// let player = {x:, y:, color:}
+	ctx.f
+}
 
 
