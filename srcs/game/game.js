49d5f6eb197de9193895559d4665dd305822053c
Jean Teissier <jteissie@made-f0Br7s5.clusters.42paris.fr>
2024-11-18 14:00:27 +0100

    batman

---
+++ srcs/game/game.js
@@ -0,0 +1,6 @@
+
+// size by default of the canvas is 300x150, it can be sized arbitrarily by CSS. If it appears distorted, best specify it in the <canvas> attribute and not the CSS
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
