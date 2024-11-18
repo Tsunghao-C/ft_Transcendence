22c599812498c0a70d2e7b1a7ff5d5e7ae2da38f
Jean Teissier <jteissie@made-f0Br7s5.clusters.42paris.fr>
2024-11-18 14:55:31 +0100

    Basic rendering of window done
    Add: gameLoop() function to render game area. Ball object to store all
    balls related information and keep track of changes. Basic logic to
    calculate ball collision, speed and direction changes.

--- srcs/game/game.js
+++ srcs/game/game.js
@@ -1,12 +1,30 @@
 
 // size by default of the canvas is 300x150, it can be sized arbitrarily by CSS. If it appears distorted, best specify it in the <canvas> attribute and not the CSS
-const canvas = document.getElementById("canvas");
-if (canvas.getContext)
-{
+const canvas = document.getElementById("game");
+if (canvas.getContext) {
 	const ctx = canvas.getContext("2d");
-	let ball = {x:canvas.width/2, y:canvas.height/2, color:"black"};
+	let ball = {x:canvas.width/2, y:canvas.height/2, color:'white', speedX:5, speedY:5, radius:10};
 	// let player = {x:, y:, color:}
-	ctx.f
+
+	function gameLoop() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ball.x += ball.speedX;
+		ball.y += ball.speedY;
+
+		if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
+			ball.speedY *= -1;
+		}
+		if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width) {
+			ball.speedX *= -1;
+		}
+		ctx.beginPath();
+		ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+		ctx.fillStyle = ball.color;
+		ctx.fill();
+		ctx.closePath();
+
+		requestAnimationFrame(gameLoop);
+	}
+
+	gameLoop();
 }
-
-
