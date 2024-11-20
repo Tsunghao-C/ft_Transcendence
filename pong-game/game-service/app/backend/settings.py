244b870d360c4a21b1288586c824533f3d8dcb2c
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-11-20 18:07:27 +0100

    attempting to use 2 databases, one for user and one for game. Can't get docker compose to work. committing this so I can continue at 42 tomorrow

--- pong-game/game-service/app/backend/settings.py
+++ pong-game/game-service/app/backend/settings.py
@@ -23,17 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "wowow321"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = "wowow321"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = bool(os.environ.get("DEBUG", default=0))
-DEBUG = 1
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+#DEBUG = 1
 
-#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = [
-	"*"
-]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = [
+#	"*"
+#]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	"user_service",
+	"game_service",
 	"rest_framework",
 	"corsheaders",
 ]
@@ -106,9 +107,18 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
+    },
+	"user_db": {
+        "ENGINE": os.environ.get("SQL_USER_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_USER_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER_USER", "user"),
+        "PASSWORD": os.environ.get("USER_DB_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_USER_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_USER_PORT", "5433"),
     }
 }
 
+DATABASE_ROUTERS = ['backend.database_router.AppDatabaseRouter']
 
 
 # Password validation
@@ -129,6 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user_service.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
