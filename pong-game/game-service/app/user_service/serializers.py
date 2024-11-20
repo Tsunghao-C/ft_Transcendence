f38db6354d25a5ce5c6f86bc9b68dca6c35f6653
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-11-20 16:55:32 +0100

    added mmr and alias to user fields

--- pong-game/game-service/app/user_service/serializers.py
+++ pong-game/game-service/app/user_service/serializers.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.models import User
+from .models import CustomUser
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = User
-		fields = ["id", "username", "password"]
-		extra_kwargs = {"password": {"write_only": True}} # we accept the password as an input but we don't return it
-	
+		model = CustomUser
+		fields = ["id", "username", "email", "password", "alias", "mmr"]
+		extra_kwargs = {
+			"password": {"write_only": True}, # we accept the password as an input but we don't return it
+			"mmr": {"read_only": True}, # used for matchmaking / leaderboards
+		}
+
 	def create(self, validated_data):
-		user = User.objects.create_user(**validated_data)
+		user = CustomUser.objects.create_user(**validated_data)
 		return user
