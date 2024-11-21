5e3c358c598b6a053b85c4cc29a114203d59bc67
ben <benjamin.h.thomas1@gmail.com>
2024-11-21 14:31:20 +0100

    added view to update user mmr

--- pong-game/game-service/app/user_service/views.py
+++ pong-game/game-service/app/user_service/views.py
@@ -15,3 +15,26 @@ class CurrentUserView(APIView):
 	def get(self, request):
 		serializer = UserSerializer(request.user)
 		return Response(serializer.data)
+	
+class UpdateMMR(APIView):
+	def post(self, request):
+		serializer = UserSerializer(request.user)
+		userMMR = serializer.data.get("mmr")
+		oppMMR = request.data.get("oppMMR")
+		# Match outcome, 1 or 0
+		S = request.data.get("matchOutcome")
+
+		if oppMMR is None or S is None:
+			return Response({"error": "oppMMR and playerWin are required"}, status=400)
+		try:
+			oppMMR = int(oppMMR)
+			S = int(S)
+		except ValueError:
+			return Response({"error": "Invalid input types"}, status=400)
+		# Calculate the 'expected score'
+		E = 1 / (1 + 10**((oppMMR - userMMR)/400))
+		# Calculate the new mmr
+		newMMR = int(userMMR + 30 * (S - E))
+		request.user.mmr = newMMR
+		request.user.save()
+		return Response({"newMMR": newMMR, "message": "MMR updated successfully"})
