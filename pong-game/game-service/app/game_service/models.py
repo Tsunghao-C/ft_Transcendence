9ff9adb63e8cf723b9041518d6e34e39d91c94af
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-11-22 14:45:25 +0100

    added table for game outcome

--- pong-game/game-service/app/game_service/models.py
+++ pong-game/game-service/app/game_service/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from user_service.models import CustomUser
 
+class MatchResults(models.Model):
+	p1 = models.ForeignKey(
+		CustomUser,
+		on_delete=models.CASCADE,
+		related_name="matches_as_p1"
+	)
+	p2 = models.ForeignKey(
+		CustomUser,
+		on_delete=models.CASCADE,
+		related_name="matches_as_p2"
+	)
+	matchoutcome = models.IntegerField(choices=[(0, 'Player 2 Wins'), (1, 'Player 1 Wins')])
