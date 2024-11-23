8ac0db9edb0828b8813fdacaf66d530cb050aa23
Jean Teissier <jteissie@made-f0Ar7s2.clusters.42paris.fr>
2024-11-23 19:27:28 +0100

    boiler plates
    cannot digest this documentation fml

--- srcs/game/gameFront.js
+++ srcs/game/gameFront.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById('game');
 
-const socket = new WebSocket('ws://gameServer:port'); 
+const socket = new WebSocket('ws://gameServer:port');  // current address for backend is localhost:8004
 // Query backend for available rooms and use that for the url
 // query client for auth token and use that as argument to url maybe? We will need to validate tho right?
 //wss for prod with SSL //get Ben to setup url for game_path
+
 
 if (canvas.getContext) {
 	const ctx = canvas.getContext('2d');
@@ -64,102 +65,14 @@ if (canvas.getContext) {
 			Player2.Paddle.speed = 0;
 	});
 
-	function updatePlayer(Player) {
-		if (Player.Paddle.speed > 0)
-		{
-			if (Player.Paddle.y + PADDLE_HEIGHT == canvas.height)	
-				return ;
-			if ((Player.Paddle.y + PADDLE_HEIGHT) + Player.Paddle.speed >= canvas.height)
-				Player.Paddle.y += canvas.height - (Player.Paddle.y + PADDLE_HEIGHT);
-			else
-				Player.Paddle.y += Player.Paddle.speed;
-		}
-		else
-		{
-			if (Player.Paddle.y == 0)
-				return ;
-			if (Player.Paddle.y - Player.Paddle.speed <= 0)
-				Player.Paddle.y += canvas.height - Player.Paddle.y;
-			else
-				Player.Paddle.y += Player.Paddle.speed;
-		}
-	}
-
-	function checkCollision(paddle) {
-		const closestX = Math.max(paddle.x, Math.min(ball.x, paddle.x + PADDLE_WIDTH));
-		const closestY = Math.max(paddle.y, Math.min(ball.y, paddle.y + PADDLE_HEIGHT));
-		
-		const distanceX = ball.x - closestX;
-		const distanceY = ball.y - closestY;
-		const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-		
-		return {
-			hasCollision: distance <= ball.radius,
-			isVertical: Math.abs(distanceY) > Math.abs(distanceX),
-			distanceX: distanceX,
-			distanceY: distanceY
-		};
-	}
-
-	function handlePaddleCollision(player) {
-		const collision = checkCollision(player.Paddle);
-		
-		if (collision.hasCollision) {
-			if (collision.isVertical) {
-				ball.speedY *= -1;
-				if (collision.distanceY > 0) {
-					ball.y = player.Paddle.y + PADDLE_HEIGHT + ball.radius;
-				} else {
-					ball.y = player.Paddle.y - ball.radius;
-				}
-			} else {
-				ball.speedX *= -1;
-				let relativeIntersection = (player.Paddle.y + (PADDLE_HEIGHT * 0.5) - ball.y);
-				let normalizedIntersection = relativeIntersection / (PADDLE_HEIGHT * 0.5);
-				ball.speedY = -normalizedIntersection * 5;
-				if (collision.distanceX > 0) {
-					ball.x = player.Paddle.x + PADDLE_WIDTH + ball.radius;
-				} else {
-					ball.x = player.Paddle.x - ball.radius;
-				}
-			}
-		}
-	}
-
-	function updateBall() {
-		ball.x += ball.speedX;
-		ball.y += ball.speedY;
-		if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height)
-			ball.speedY *= -1;
-		if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width)
-		{
-			if (ball.x - ball.radius < 0)
-			{
-				Player2.score++;
-				ball.x = canvas.width * 0.7;
-				ball.y = canvas.height * 0.5;
-			}
-			else
-			{
-				Player1.score++;
-				ball.x = canvas.width * 0.3;
-				ball.y = canvas.height * 0.5;
-			}
-//			ball.speedX *= -1;
-		}
-	}
+	
 	
 	function gameLoop() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.font = '48px serif';
-		ctx.textBaseline = 'hanging';
-		ctx.fillStyle = 'white';
+//		ctx.font = '48px serif';
+//		ctx.textBaseline = 'hanging';
+//		ctx.fillStyle = 'white';
 		ctx.fillText(Player1.score + " : " + Player2.score, canvas.width * 0.45, canvas.height * 0.10);
-		updatePlayer(Player1);
-		updatePlayer(Player2);
-		handlePaddleCollision(Player1);
-		handlePaddleCollision(Player2);
-		updateBall();
 		ctx.fillStyle = Player1.color;
 		ctx.fillRect(Player1.Paddle.x, Player1.Paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
 		ctx.fillStyle = Player2.color;
---
+++ srcs/game/privateMatch.js
@@ -0,0 +1,16 @@
+//class Player {
+//		constructor (id, color) {
+//			this.id = id;
+//			this.color = color;
+//			this.Paddle = new Paddle(id, color);
+//			this.score = 0;
+//		}
+//	}
+
+	async function getPlayerInfo() {
+
+	}
+
+	async function requestRoom() {
+
+	}
