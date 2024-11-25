36c8c6c92141abdf7e60d41bd3cfe1a290e32b71
Jean Teissier <jteissie@made-f0Br7s3.clusters.42paris.fr>
2024-11-25 11:36:29 +0100

    Prototype for fetch playerInfo
    Add: Prototype method for getPlayerInfo() making request to Django api.

--- srcs/game/privateMatch.js
+++ srcs/game/privateMatch.js
@@ -8,9 +8,29 @@
 //	}
 
 	async function getPlayerInfo() {
-
+	try {
+		const dbQuery = await fetch('http://django/api/player/info/', {
+			method: 'GET',
+			headers: {
+				'Auth' : `Bearer ${token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+		if (!dbQuery.ok) {
+			throw new Error('Failed to fetch player info');
+		}
+		const playerInfo = await dbQuery.json();
+		return playerInfo;
+	} catch (error) {
+		console.error('Error fetching player info:', error);
+		throw error;
+	}
 	}
 
 	async function requestRoom() {
+
+	}
+
+	function setupRoom() {
 
 	}
