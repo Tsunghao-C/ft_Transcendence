82eed726537565855625ca7baf3b271a52c2760e
Ben <benjamin.h.thomas1@gmail.com>
2024-11-27 12:08:44 +0100

    replaced login view

--- pong-game/game-service/app/user_service/urls.py
+++ pong-game/game-service/app/user_service/urls.py
@@ -6,13 +6,15 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path("/register/", CreateUserView.as_view(), name="register"),
 	# path("/token/", TokenObtainPairView.as_view(), name="get_token"),
-	path("/token/", Generate2FAView.as_view(), name="generate_2fa"),
+	
 	path("/token/validate/", Validate2FAView.as_view(), name="validate_2fa"),
 	path("/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("/getuser/", CurrentUserView.as_view(), name="get_user"),
     path("/updatemmr/", UpdateMMR.as_view(), name="update_mmr"),
 	path("/banplayer/", BanPlayer.as_view(), name="banplayer"),
 	path("/unbanplayer/", UnbanPlayer.as_view(), name="unbanplayer"),
-    path("/login/", LoginView.as_view(template_name="user_service/login.html"), name="login-user"),
+
+    # path("/login/", LoginView.as_view(template_name="user_service/login.html"), name="login-user"),
+	path("/login/", Generate2FAView.as_view(template_name="user_service/login.html"), name="generate_2fa"),
     path("/logout/", LogoutView.as_view(), name="logout-user"),
 ]
