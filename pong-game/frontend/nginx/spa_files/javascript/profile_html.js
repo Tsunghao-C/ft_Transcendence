5f4bbc283fab835c4f82f1e0e2adb81b117f8b9b
asut00 <alexandresuteau@gmail.com>
2024-12-02 10:33:30 +0100

    update login page style

--- pong-game/frontend/nginx/spa_files/javascript/profile_html.js
+++ pong-game/frontend/nginx/spa_files/javascript/profile_html.js
@@ -1,39 +1,73 @@
+// async function loadHtmlxxx(url) {
+//     try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error(`Failed to load ${url}: ${response.statusText}`);
+//         }
+//         const text = await response.text();
+//         console.log(`Content loaded from ${url}:\n\n\n`, text); // Log pour vérifier le contenu
+//         return text;
+//     } catch (error) {
+//         console.error(`Error loading ${url}:`, error);
+//         return ''; // Retourne une chaîne vide en cas d'erreur
+//     }
+// }
+
+// export async function setLoginViewHtml(contentContainer) {
+//     try {
+//         const htmlContent = await loadHtmlxxx('./login_view.html'); // Remplacez par le chemin réel
+//         contentContainer.innerHTML = htmlContent;
+//     } catch (error) {
+//         console.error('Error loading login view:', error);
+//     }
+// }
+
+// export async function setRegisterViewHtml(contentContainer) {
+//     try {
+//         const htmlContent = await loadHtmlxxx('./register_view.html'); // Remplacez par le chemin réel
+//         contentContainer.innerHTML = htmlContent;
+//     } catch (error) {
+//         console.error('Error loading register view:', error);
+//     }
+// }
+
 export function setLoginViewHtml(contentContainer) {
-    contentContainer.innerHTML = `
-        <div class="login-view">
-            <h1 data-i18n="loginTitle">Login</h1>
-            <form id="loginForm">
-                <div class="mb-3">
-                    <label for="usernameInput" class="form-label" data-i18n="username">Username</label>
-                    <input type="text" class="form-control" id="usernameInput" placeholder="Enter your username">
-                </div>
-                <div class="mb-3">
-                    <label for="passwordInput" class="form-label" data-i18n="password">Password</label>
-                    <input type="password" class="form-control" id="passwordInput" placeholder="Enter your password">
-                </div>
-                <div>
-                    <p id="errorMessage"></p>
-                    <p id="successMessage"></p>
-                </div>
-                <button type="submit" class="btn btn-primary" data-i18n="loginButton">Log In</button>
-            </form>
-            <form id="2faForm">
-                <div class="mb-3" id="2faDiv" style="display: none;">
-                    <label for="2faInput" class="form-label" data-i18n="2fa">2FA Code</label>
-                    <input type="text" class="form-control" id="2faInput" placeholder="Enter 2FA Code">
-                    <button type="submit" class="btn btn-primary" class="login-button">Verify 2FA</button>
-                </div>
-            </form>
-            <p data-i18n="noAccount">Don’t have an account?
-                <a href="#register" id="registerLink" data-i18n="registerLink">Create one here</a>
-            </p>
-        </div>
-    `;
+	contentContainer.innerHTML = `
+		<div class="centered-box login-view">
+			<h1 id="block-title">ponGx</h1>
+			<h2 data-i18n="loginTitle">Login</h2>
+			<form id="loginForm">
+				<div class="mb-3">
+					<label for="usernameInput" class="form-label" data-i18n="username">Username</label>
+					<input type="text" class="form-control" id="usernameInput" placeholder="Enter your username">
+				</div>
+				<div class="mb-3">
+					<label for="passwordInput" class="form-label" data-i18n="password">Password</label>
+					<input type="password" class="form-control" id="passwordInput" placeholder="Enter your password">
+				</div>
+				<div>
+					<p id="errorMessage"></p>
+					<p id="successMessage"></p>
+				</div>
+				<button type="submit" class="btn btn-light" data-i18n="loginButton">Log In</button>
+			</form>
+			<form id="2faForm">
+				<div class="mb-3" id="2faDiv" style="display: none;">
+					<label for="2faInput" class="form-label" data-i18n="2fa">2FA Code</label>
+					<input type="text" class="form-control" id="2faInput" placeholder="Enter 2FA Code">
+					<button type="submit" class="btn btn-primary" class="login-button">Verify 2FA</button>
+				</div>
+			</form>
+			<p data-i18n="noAccount">Don’t have an account?
+				<a href="#register" id="registerLink" data-i18n="registerLink">Create one here</a>
+			</p>
+		</div>
+	`;
 }
 
 export function setRegisterViewHtml(contentContainer) {
 	contentContainer.innerHTML = `
-		<div class="register-view">
+		<div class="centered-box register-view">
 			<h1 data-i18n="registerTitle">Register</h1>
 			<form id="registerForm">
 				<div class="mb-3">
@@ -63,4 +97,5 @@ export function setRegisterViewHtml(contentContainer) {
 				<button type="submit" class="btn btn-primary" data-i18n="registerButton">Register</button>
 			</form>
 		</div>
-	`;}
+	`;
+}
