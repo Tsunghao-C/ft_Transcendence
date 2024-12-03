a804fdab981c95605fe6269fd8044b4931b5e4a3
Ben <benjamin.h.thomas1@gmail.com>
2024-12-03 15:27:14 +0100

    temp fix for routing

--- pong-game/game-service/app/backend/settings.py
+++ pong-game/game-service/app/backend/settings.py
@@ -179,6 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #	"http://0.0.0.0:8000"
 #]
 #CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # can replace inmemorychannellayer with redis later
 CHANNEL_LAYERS = {
@@ -190,7 +191,7 @@ CHANNEL_LAYERS = {
     },
 }
 
-LOGIN_REDIRECT_URL = '/chat/'
+# LOGIN_REDIRECT_URL = '/chat/'
 
 # Alex add for Email host setup
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
