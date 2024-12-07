03e50e5ec4903077ef23704c8596ff61fe512d39
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-12-07 10:38:14 +0100

    changed updateLeaderboard into a class method and changed main loop to use atomic for data safety

--- pong-game/game-service/app/game_service/apps.py
+++ pong-game/game-service/app/game_service/apps.py
@@ -1,24 +1,7 @@
 from django.apps import AppConfig
 from .models import LeaderBoard
 from user_service.models import CustomUser
-from django.db.models import F
-from django.utils.timezone import now
 
 class GameServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'game_service'
-
-def updateLeaderBoard():
-    players = CustomUser.objects.annotate(
-        mmr=F('mmr'),
-        wins=F('winCount'),
-    ).order_by('-mmr', '-wins') # first by mmr and then by wins
-
-    LeaderBoard.objects.all().delete() # delete old entries
-
-    for rank, player in enumerate(players, start=1):
-        LeaderBoard.objects.create(
-            rank=rank,
-            player=player,
-        )
-    return now()
--- pong-game/game-service/app/game_service/models.py
+++ pong-game/game-service/app/game_service/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from django.db import models, transaction
 from user_service.models import CustomUser
+from django.db.models import F
+from django.utils.timezone import now
 
 class MatchResults(models.Model):
 	p1 = models.ForeignKey(
@@ -7,6 +9,11 @@ class MatchResults(models.Model):
 		on_delete=models.CASCADE,
 		related_name="matches_as_p1"
 	)
+
+	@property
+	def p1Alias(self):
+		return self.p1.alias
+
 	p2 = models.ForeignKey(
 		CustomUser,
 		on_delete=models.CASCADE,
@@ -38,3 +45,16 @@ class LeaderBoard(models.Model):
 	@property
 	def losses(self):
 		return self.player.lossCount
+
+	@classmethod
+	def updateLeaderBoard(cls):
+		players = CustomUser.objects.order_by('-mmr', '-wins') # first by mmr and then by wins
+
+		with transaction.atomic(): # blocks all other write operations for db & if this code block fails, rewinds to before function call
+			cls.objects.all().delete()
+			leaderboard_entries = [
+				cls(rank=rank, player=player)
+				for rank, player in enumerate(players, start=1)
+			]
+			cls.objects.bulk_create(leaderboard_entries)
+		return now()
