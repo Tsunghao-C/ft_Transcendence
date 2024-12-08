1efbb0534a103cdfc710f3c1f0e05369885cc199
ben <benjamin.h.thomas1@gmail.com>
2024-12-08 19:00:38 +0100

    added function to record match

--- pong-game/game-service/app/game_service/views.py
+++ pong-game/game-service/app/game_service/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import MatchResults
 
 # Create your views here.
+def recordMatch(p1, p2, matchOutcome):
+    match = MatchResults.objects.create(
+        p1=p1,
+        p2=p2,
+        matchOutcome=matchOutcome
+    )
+    return match
