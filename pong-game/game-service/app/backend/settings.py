a8697d9c622295a5a4c286abf9d452dc65d06f79
Tsunghao <tsunghao.chen12@gmail.com>
2024-12-09 22:20:48 +0100

    cleaned up some comments

--- pong-game/game-service/app/backend/settings.py
+++ pong-game/game-service/app/backend/settings.py
@@ -56,7 +56,6 @@ SIMPLE_JWT = {
 INSTALLED_APPS = [
     "daphne",
     "chat",
-    # "channels",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pong-game/game-service/app/game_service/urls.py
+++ pong-game/game-service/app/game_service/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # WS testing path
     path('test/<str:game_id>/', views.game_test, name='game_test'),
 ]
