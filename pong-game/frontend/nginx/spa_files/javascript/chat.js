230a6b433241b6b4554fc9b915fe2535f5cc891c
asut00 <alexandresuteau@gmail.com>
2024-12-10 15:03:25 +0100

    add about and chat.js

---
+++ pong-game/frontend/nginx/spa_files/javascript/chat.js
@@ -0,0 +1,9 @@
+
+export function setChatPage(contentContainer) {
+	contentContainer.innerHTML = `
+		<div class="chat-view">
+			<h2 data-i18n="chat">Chat</h2>
+			<p>This is a chatting interface.</p>
+		</div>
+		`;
+}
