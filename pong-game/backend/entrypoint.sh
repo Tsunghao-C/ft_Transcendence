5a33d0623fa902c2511cb692b8ad2246bf39e798
Ben <benjamin.h.thomas1@gmail.com>
2024-12-11 14:02:31 +0100

    added env to crontab

--- pong-game/backend/entrypoint.sh
+++ pong-game/backend/entrypoint.sh
@@ -25,6 +25,8 @@ python manage.py migrate user_service
 python manage.py migrate game_service
 
 echo "Adding django crontab..."
+touch /var/log/cron.log
+printenv | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID|LANG|PWD|GPG_KEY|_=' >> /etc/environment
 python manage.py crontab remove
 python manage.py crontab add || { echo "failed to add crontab"; exit 1; }
 python manage.py crontab show || { echo "failed to show crontab"; exit 1; }
