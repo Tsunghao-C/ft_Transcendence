ae4c17517b322324dad2eedcf00e44372066d039
Ben <benjamin.h.thomas1@gmail.com>
2024-12-11 15:57:50 +0100

    use user serializer to get all user fields

--- pong-game/backend/app/game_service/serializers.py
+++ pong-game/backend/app/game_service/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import LeaderBoard, MatchResults
+from user_service.serializers import UserSerializer
 
 class LeaderBoardSerializer(serializers.ModelSerializer):
+    player = UserSerializer()
     class Meta:
         model = LeaderBoard
-        fields = '__all__'
+        fields = ['rank', 'player']
 
 class matchResultsSerializer(serializers.ModelSerializer):
     time = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
