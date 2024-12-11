27e133fa53abd7638cb9980f830cd6597575220d
Atwazar <theo.helaine@gmail.com>
2024-12-11 15:26:51 +0100

    match history added, learderboard checked, to be integrated in front end in next pr

--- pong-game/frontend/nginx/spa_files/javascript/leaderboard.js
+++ pong-game/frontend/nginx/spa_files/javascript/leaderboard.js
@@ -1,11 +1,23 @@
 //// We may fuse this with the search bar of profile view so that we can search for someone in the leaderboard
-import { getLanguageCookie } from './fetch_request.js';
+import { fetchWithToken, getLanguageCookie } from './fetch_request.js';
 import { translations } from './language_pack.js';
 
 import { setContainerHtml } from './app.js'
 
 export async function setLeaderboardView(contentContainer) {
 	// await setContainerHtml(contentContainer, "./html/leaderboard.html");
+	let response;
+	try {
+		const response = await fetchWithToken('/api/game/leaderboard/');
+		const leaderboardData = await response.json();
+		console.leaderboardData;
+		populateLeaderboardTable(leaderboardData);
+	} catch (error) {
+		console.error("Failed to fetch leaderboard data:", error);
+		const tableBody = document.getElementById("leaderboardTableBody");
+		tableBody.innerHTML = '<tr><td colspan="3">Error loading leaderboard</td></tr>';
+	}
+
 	contentContainer.innerHTML = `
 		<div class="leaderboard-view">
 			<h2 data-i18n="leaderboard">Leaderboard</h2>
@@ -26,29 +38,6 @@ export async function setLeaderboardView(contentContainer) {
 }
 
 
-//v1 of async function to get data using apis
-// async function fetchLeaderboardData() {
-// 	try {
-// 		const response = await fetch('/api/leaderboard');
-// 		const leaderboardData = await response.json();
-
-// 		populateLeaderboardTable(leaderboardData);
-// 	} catch (error) {
-// 		console.error("Failed to fetch leaderboard data:", error);
-// 		const tableBody = document.getElementById("leaderboardTableBody");
-// 		tableBody.innerHTML = '<tr><td colspan="3">Error loading leaderboard</td></tr>';
-// 	}
-// }
-
-const playerData = [
-	{ username: "Player1", mmr: 1500 },
-	{ username: "Player2", mmr: 1450 },
-	{ username: "Player3", mmr: 1400 },
-];
-
-function fetchLeaderboardData() {
-	populateLeaderboardTable(playerData);
-}
 
 function populateLeaderboardTable(data) {
 	const tableBody = document.getElementById("leaderboardTableBody");
