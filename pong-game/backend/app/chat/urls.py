934b14ea2cb4d6e18425151e20ef82931ebb3581
Atwazar <theo.helaine@gmail.com>
2024-12-13 16:01:34 +0100

    public room chat working

--- pong-game/backend/app/chat/urls.py
+++ pong-game/backend/app/chat/urls.py
@@ -5,9 +5,9 @@ from .views import ChatRoomMessages, CreateChatRoom, CreatePrivateRoomView
 
 
 urlpatterns = [
-    # path("", views.lobby),
-    # path("index/", views.index, name="index"),
-    # path("<str:room_name>/", views.room, name="room"),
+	# path("", views.lobby),
+	# path("index/", views.index, name="index"),
+	# path("<str:room_name>/", views.room, name="room"),
 	path('rooms/<str:room_name>/messages/', ChatRoomMessages.as_view(), name='chat_room_messages'),
 	path('rooms/create/', CreateChatRoom.as_view(), name='create_chat_room'),
 	path('rooms/create-private/', CreatePrivateRoomView.as_view(), name='create_private_chat_room'),
