0c88591c6ac3cb19b15f5895a9596fa7a610ac8c
Atwazar <theo.helaine@gmail.com>
2024-12-15 16:33:53 +0100

    eed to use my brain

--- pong-game/backend/app/chat/urls.py
+++ pong-game/backend/app/chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-# from . import views
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import getChatRoomMessagesView, CreatePublicChatRoomView, CreatePrivateChatRoomView, getChatRoomsOfUserView
 
