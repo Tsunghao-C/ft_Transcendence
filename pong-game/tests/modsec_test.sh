11b30110592b866a4a35e386578ac22efea7e091
Tsunghao <tsunghao.chen12@gmail.com>
2024-12-17 16:13:48 +0100

    Moved test script and updated makefile rule

--- pong-game/tests/modsec_test.sh
+++ pong-game/tests/modsec_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 URL="https://localhost:8443"
 
 CURL_OPTIONS="-k -s -o /dev/null -w '%{http_code}'"
@@ -25,7 +30,7 @@ if [ "$response" == "403" ]; then
     echo "Blocked ✅ (403 Forbidden)"
 else
     echo "Not blocked ❌ (HTTP $response)"
-    error_occurred=1
+    error_occurred=2
 fi
 
 # Test 3 : User-Agent suspecte
@@ -35,7 +40,7 @@ if [ "$response" == "403" ]; then
     echo "Blocked ✅ (403 Forbidden)"
 else
     echo "Not blocked ❌ (HTTP $response)"
-    error_occurred=1
+    error_occurred=3
 fi
 
 # Test 4 : XSS Injection
@@ -45,7 +50,7 @@ if [ "$response" == "403" ]; then
     echo "Blocked ✅ (403 Forbidden)"
 else
     echo "Not blocked ❌ (HTTP $response)"
-    error_occurred=1
+    error_occurred=4
 fi
 
 # Test 5 : Command Injection
@@ -55,16 +60,16 @@ if [ "$response" == "403" ]; then
     echo "Blocked ✅ (403 Forbidden)"
 else
     echo "Not blocked ❌ (HTTP $response)"
-    error_occurred=1
-fi
-
-# Final result
-if [ $error_occurred -eq 1 ]; then
-    echo "An error has been detected"
-    exit 1
-else
-    echo "All tests passed"
-    exit 0
+    error_occurred=5
 fi
 
 echo "=== Tests over ==="
+
+# Final result
+if [ ! $error_occurred -eq 0 ]; then
+    echo -e "${RED}An error has been detected${NC}"
+    exit 1
+else
+    echo -e "${GREEN}All WAF tests passed!${NC}"
+    exit 0
+fi
