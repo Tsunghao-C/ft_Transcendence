740ee3bea1c83d9ee0ce10c045c5ae063db6f414
asut00 <alexandresuteau@gmail.com>
2024-12-18 14:54:38 +0100

    cleaning

--- pong-game/frontend/nginx/spa_files/javascript/login_login.js
+++ pong-game/frontend/nginx/spa_files/javascript/login_login.js
@@ -17,7 +17,6 @@ function show2FAInput() {
 
 ///////////////////// API Calls /////////////////////
 
-// Bring back 2FA
 async function loginUserInBackend(username, password) {
 	const response = await fetch('/api/user/2FA/generate/', {
 		method: 'POST',
@@ -38,11 +37,8 @@ async function verify2FAInBackend(user_id, otpCode) {
 
 ///////////////////// Event Handlers /////////////////////
 
-//TO bipass 2fa error
-function setupLoginFormEventHandler() {
+function loginFormEventHandler() {
 	const loginForm = document.getElementById("loginForm");
-	const languageSelect = document.getElementById("languageSelect");
-	languageSelect.value = getLanguageCookie() || "en";
 	setCustomValidation("usernameInput");
 	setCustomValidation("passwordInput");
 	loginForm.addEventListener("submit", async (event) => {
@@ -69,14 +65,9 @@ function setupLoginFormEventHandler() {
 			showError('Login failed. Please try again.');
 		}
 	});
-	languageSelect.addEventListener("change", async (event) => {
-		const selectedLanguage = event.target.value;
-		setLanguageCookie(selectedLanguage);
-		loadPage("login");
-	});
 }
 
-function setup2FAFormEventHandler() {
+function twoFAFormEventHandler() {
 	const twoFAForm = document.getElementById("2faForm");
 	if (twoFAForm) {
 		twoFAForm.addEventListener("submit", async (event) => {
@@ -88,7 +79,6 @@ function setup2FAFormEventHandler() {
 				const data = await response.json();
 				if (data.detail === "2FA code validated") {
 					console.log("2FA code has been validated by backend");
-
 					console.log("trying to save the cookies");
 					document.cookie = `accessToken=${data.access}; path=/; secure; SameSite=Strict`;
 					document.cookie = `refreshToken=${data.refresh}; path=/; secure; SameSite=Strict`;
@@ -103,18 +93,25 @@ function setup2FAFormEventHandler() {
 				showError('An error occurred during 2FA verification.');
 			}
 		})
-		languageSelect.addEventListener("change", async (event) => {
-			const selectedLanguage = event.target.value;
-			setLanguageCookie(selectedLanguage);
-			loadPage("login");
-		});
 	}
+}
+
+function languageEventListener() {
+	const languageSelect = document.getElementById("languageSelect");
+	languageSelect.value = getLanguageCookie() || "en";
+	languageSelect.addEventListener("change", async (event) => {
+		const selectedLanguage = event.target.value;
+		setLanguageCookie(selectedLanguage);
+		loadPage("login");
+		show2FAInput();
+	});
 }
 
 ///////////////////// Main function /////////////////////
 
 export function setLoginView(contentContainer) {
 	setLoginViewHtml(contentContainer);
-	setupLoginFormEventHandler();
-	setup2FAFormEventHandler();
+	loginFormEventHandler();
+	twoFAFormEventHandler();
+	languageEventListener();
 }
