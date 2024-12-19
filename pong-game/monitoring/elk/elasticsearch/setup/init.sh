140aa31ac2714934aa5cab7620e6c1ac408792c9
Tsunghao <tsunghao.chen12@gmail.com>
2024-12-19 14:55:01 +0100

    Added admin user to ELK. Now you need to log in as admin to visit kibana. What a progress

--- pong-game/monitoring/elk/elasticsearch/setup/init.sh
+++ pong-game/monitoring/elk/elasticsearch/setup/init.sh
@@ -2,11 +2,28 @@
 
 # Wait for Elasticsearch to be ready
 until curl -s http://localhost:9200 >/dev/null; do
-    echo "Waiting for Elasticsearch..."
+    echo "Waiting for Elasticsearch to be ready..."
     sleep 5
 done
 
 echo "Elasticsearch is up - initializing..."
+
+echo "Setting up built-in users..."
+
+# Set up elastic superuser password
+echo "Setting elastic user password..."
+curl -X POST -H "Content-Type: application/json" \
+     "http://localhost:9200/_security/user/elastic/_password" \
+     -d "{\"password\":\"${ELASTIC_PASSWORD}\"}"
+
+# Set up kibana_system user
+echo "Setting kibana_system user password..."
+curl -X POST -H "Content-Type: application/json" \
+     -u "elastic:${ELASTIC_PASSWORD}" \
+     "http://localhost:9200/_security/user/kibana_system/_password" \
+     -d "{\"password\":\"${ELASTIC_PASSWORD}\"}"
+
+echo "User setup complete"
 
 # Source other scripts
 DIR="$(dirname "${BASH_SOURCE[0]}")"
