2b275c269d34b2e8074c0b76901f6d85f6359e26
Tsunghao <tsunghao.chen12@gmail.com>
2024-12-21 00:29:27 +0100

    Added internal SSL TLS setting and can run. But the data sent from lgstash had issue

--- pong-game/monitoring/elk/create-certs.sh
+++ pong-game/monitoring/elk/create-certs.sh
@@ -14,13 +14,45 @@ openssl req -new -key $CERT_DIR/kibana.key -out $CERT_DIR/kibana.csr -subj "/CN=
 openssl x509 -req -days 365 -in $CERT_DIR/kibana.csr \
     -signkey $CERT_DIR/kibana.key -out $CERT_DIR/kibana.crt
 
-rm $CERT_DIR/kibana.csr
+# rm $CERT_DIR/kibana.csr
 
-# Set proper permissions
-chmod 444 $CERT_DIR/kibana.key
-chmod 444 $CERT_DIR/kibana.crt
+# # Set proper permissions
+# chmod 444 $CERT_DIR/kibana.key
+# chmod 444 $CERT_DIR/kibana.crt
 
-echo "SSL certificates generated successfully"
+echo "Kibana SSL certificates generated successfully"
+
+## Generate ELK internal certificates
+# Create CA certificate
+openssl genrsa -out $CERT_DIR/ca.key 4096
+openssl req -new -x509 -days 365 -key $CERT_DIR/ca.key \
+    -out $CERT_DIR/ca.crt \
+    -subj "/CN=elk-ca/O=Elastic/C=US"
+
+# Create transport certificates for Elasticsearch
+openssl genrsa -out $CERT_DIR/elastic-transport.key 2048
+openssl req -new -key $CERT_DIR/elastic-transport.key \
+    -out $CERT_DIR/elastic-transport.csr \
+    -subj "/CN=elasticsearch/O=Elastic/C=US"
+
+openssl x509 -req -days 365 -in $CERT_DIR/elastic-transport.csr \
+    -CA $CERT_DIR/ca.crt -CAkey $CERT_DIR/ca.key -CAcreateserial \
+    -out $CERT_DIR/elastic-transport.crt
+
+# Create HTTP certificates for Elasticsearch
+openssl genrsa -out $CERT_DIR/elastic-http.key 2048
+openssl req -new -key $CERT_DIR/elastic-http.key \
+    -out $CERT_DIR/elastic-http.csr \
+    -subj "/CN=elasticsearch/O=Elastic/C=US"
+
+openssl x509 -req -days 365 -in $CERT_DIR/elastic-http.csr \
+    -CA $CERT_DIR/ca.crt -CAkey $CERT_DIR/ca.key -CAcreateserial \
+    -out $CERT_DIR/elastic-http.crt
+
+rm $CERT_DIR/*.csr
+
+chmod 444 $CERT_DIR/*.key
+chmod 444 $CERT_DIR/*.crt
 
 ## Auto ssl creation script
 # CERT_DIR="/usr/share/kibana/config/certs"
