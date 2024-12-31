2eb67963159487d0e2e057fd884a37b007484899
asut00 <alexandresuteau@gmail.com>
2024-12-31 19:16:31 +0100

    update game_local ux and display

--- pong-game/frontend/nginx/spa_files/javascript/game_local.js
+++ pong-game/frontend/nginx/spa_files/javascript/game_local.js
@@ -5,6 +5,7 @@ import { state } from "./app.js";
 import { hideElem } from "./utils.js";
 import { showElem } from "./utils.js";
 import { setUpTwoPlayersControl, connectWebSocket, playerEvent, setTypeOfGame } from "./game_utils.js";
+import { translations as trslt } from "./language_pack.js";
 
 
 export async function setLocalLobby(contentContainer, skip = false) {
@@ -22,27 +23,26 @@ export async function setLocalLobby(contentContainer, skip = false) {
         window.location.hash = "login";
         return;
     }
+    const lng = getLanguageCookie() || "en";
     contentContainer.innerHTML = `
         <div class="gamelobby-view">
         <div id="game-lobby" class="gamelobby-view">
-            <div>
-                <button id="local">start local versus</button>
-                <button id="menu">Back To Menu</button>
-                <button id="ready-button" style="display:none;">Ready</button>
-                <div id="player-info-container" style="display: flex; justify-content: space-between;">
-                    <div id="user-info-left" style="text-align: left; flex: 1; padding: 10px;"></div>
-                    <div id="user-info-right" style="text-align: left; flex: 1; padding: 10px;"></div>
-                </div>
-                <div id="game-info">Loading...</div>
-                <div id="player-status" class="player-status"></div>
-                <canvas id="game" width="800" height="600" style="display: none;"></canvas>
+            <button id="local">Start local versus</button>
+            <button id="menu">${trslt[lng].back}</button>
+            <div id="player-info-container" style="display: flex; justify-content: space-between;">
+                <div id="user-info-left" style="text-align: left; flex: 1; padding: 10px;"></div>
+                <div id="user-info-right" style="text-align: left; flex: 1; padding: 10px;"></div>
             </div>
+            <div id="game-info">Loading...</div>
+            <div id="player-status" class="player-status"></div>
+            <canvas id="game" width="800" height="600" style="display: none;"></canvas>
+            <button id="go-back-EOG" style="display: none;">${trslt[lng].back}</button>
         </div>
     `;
+
     const canvas = document.getElementById('game');
     const ctx = canvas.getContext('2d');
     const gameInfo = document.getElementById('game-info');
-
 
     async function request_local_room() {
         try {
@@ -60,9 +60,7 @@ export async function setLocalLobby(contentContainer, skip = false) {
     async function create_local_match() {
         try {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            canvas.style.display = 'block';
             await request_local_room();
-            gameInfo.textContent = 'Playing against your Friend - Get Ready!';
         } catch (error) {
             console.error('Error starting Local game:', error);
             gameInfo.textContent = 'Error starting Local game. Plase try again.';
@@ -75,19 +73,26 @@ export async function setLocalLobby(contentContainer, skip = false) {
     localButton.addEventListener('click', async () => {
         try {
             hideElem("local");
+            hideElem("menu");
             create_local_match();
         } catch (error) {
             console.error('Error in join-match event listener:', error);
         }
     });
     
-    document.getElementById('menu').addEventListener('click', async () => {
-        window.location.hash = "game/";
-    });
     await connectWebSocket();
     setUpTwoPlayersControl();
     if (skip) {
         hideElem("local");
         create_local_match();
     }
+    
+    document.getElementById('menu').addEventListener('click', async () => {
+        window.location.hash = "game/local";
+    });
+
+    document.getElementById('go-back-EOG').addEventListener('click', async () => {
+        window.location.hash = "game/local";
+    });
+
 }
